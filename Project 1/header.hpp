4f3ad6b8226d2f3ae907d521418e995376ef5ff6
AliDzulfiqar <66514385+AliDzulfiqar@users.noreply.github.com>
2023-02-14 20:10:59 -0800

    made submission

--- Project 1/header.hpp
+++ Project 1/header.hpp
@@ -11,29 +11,13 @@ struct DP_Cell {
     int sScore;
     int dScore;
     int iScore;
-    int direction;
-
-    DP_Cell(int s, int d, int i)
-    {
-        this->sScore = s;
-        this->dScore = d;
-        this->iScore = i;
-    }
-
-    DP_Cell()
-    {
-        this->sScore = 0;
-        this->dScore = 0;
-        this->iScore = 0;
-    }
 };
 
-
+int checkMatch(char a, char b);
 
 // void readInput(std::string);
 // void readConfig(std::string);
 int getMaxOf3Int(int, int, int);
 int getMaxOrZero(int, int, int);
-std::vector<std::vector<DP_Cell> > NeedlemanWunsch(std::string, std::string);
 
 #endif
--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -1,50 +1,65 @@
 #include "header.hpp"
 
 // Define globals
-std::vector<std::vector<DP_Cell> > DP_Table;
 std::string s1, s2, s1Name, s2Name;
-int match = 1, mismatch = -1, h = 0, g = -2;
-int totalMatch, totalMismatch, gapOpen, gapExtend;
+int match = 1, mismatch = -2, h = -5, g = -2;
 
 int main(int argc, char* argv[])
 {
     std::ifstream infile;
     std::string line;
-    infile.open("Input.fasta");
+    infile.open("Opsin1_colorblindness_gene.fasta");
     if (infile.is_open()){
+        // read name for s1
         getline(infile, line);
         s1Name = line;
-        std::cout << s1Name << std::endl;
 
+        // read sequence for s1
         getline(infile, line);
         while (line != ""){
             s1 += line;
             getline(infile, line);
         }
-        std::cout << s1 << std::endl;
 
+        // read name for s2
         getline(infile, line);
         s2Name = line;
-        std::cout << s2Name << std::endl;
 
+        // read sequence for s2
         getline(infile, line);
         while(!infile.eof()){
             s2 += line;
             getline(infile, line);
         }
-        std::cout << s2 << std::endl;
+        getline(infile, line);
+        s2+= line;
+
         infile.close();
     }
     else
     {
         std::cout << "Cannot open file" << std::endl;
-    }
-
+    } 
     
+    // Printing report.txt
+    std::ofstream outfile;
+    outfile.open("Report.txt");
+    outfile << "INPUT:\n******\n";
+    outfile << std::endl;
+    outfile << s1Name << "\n" << s1 << std::endl;
+    outfile << std::endl;
+    outfile << s2Name << "\n" << s2 << std::endl;
+    outfile << std::endl;
+    outfile << "OUTPUT:\n********\n";
+    outfile << std::endl;
+    outfile << "Scores:\t" << "match = " << match << ", mismatch = " << mismatch << ", h = " << h << ", g = " << g << std::endl;
+    outfile << std::endl;
+    outfile << "Sequence 1 = 's1', length = " << s1.length() << std::endl;
+    outfile << "Sequence 2 = 's2', length = " << s2.length() << std::endl;
+    outfile << std::endl;
+    outfile << "Report:" << std::endl;
 
-    // std::ofstream outfile;
-    // outfile.open("Report.txt");
-    // outfile << "Hi";
-    // outfile.close();
+
+    outfile.close();
     
 }
